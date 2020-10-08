b46bca291f638e6c9f899cecececb43b2774c0e0
icarodgl <icarodgl@gmail.com>
2020-10-07 22:18:05 -0300

    usando como servidor para o adotei

---
+++ precache-manifest.f2728cefd3b8ed4e97a8335f8d7c8271.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f74aacd3fffab7921b6e6c750c025a7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c11c64c8a489da08e98d",
+    "url": "/static/css/main.06d32c36.chunk.css"
+  },
+  {
+    "revision": "bca943ba961abb08ebbd",
+    "url": "/static/js/2.feaa8828.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.feaa8828.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c11c64c8a489da08e98d",
+    "url": "/static/js/main.f68c0145.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "4f6669bea244ad6c2617d18fcb21921b",
+    "url": "/static/media/logodotei.4f6669be.png"
+  },
+  {
+    "revision": "db9c6cf89fe927ea9abc72bf426f19a9",
+    "url": "/static/media/viralataCaramel1.db9c6cf8.jpg"
+  }
+]);
